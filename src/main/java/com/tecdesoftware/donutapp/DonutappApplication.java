8bf9e6d8f1aa7f1797a870d38f43aa7bef601d94
Jose Samos <chemasassss@gmail.com>
2025-06-06 11:06:57 -0600

    Primer commit de mi Donut App 🍩

---
+++ src/main/java/com/tecdesoftware/donutapp/DonutappApplication.java
@@ -0,0 +1,13 @@
+package com.tecdesoftware.donutapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonutappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonutappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tecdesoftware/donutapp/DonutappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecdesoftware.donutapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonutappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
